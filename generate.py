acad9c503c00fba4bfb662d94076640bace9affe
Donald Whyte <donaldwhyte0@gmail.com>
2016-09-25 21:35:21 +0100

    Add script to generate new project boilerplate from template

---
+++ generate.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python
+
+import argparse
+import os
+import re
+import shutil
+
+TEMPLATE_DIR = 'project_template'
+PACKAGE_NAME_PLACEHOLDER  = 'PACKAGE_NAME'
+PACKAGE_NAME_REGEX = re.compile(r'^\w+$')
+
+
+def getAllFilesInDir(directory):
+    files = []
+    dirs = []
+    for root, directories, filenames in os.walk(directory):
+        for f in filenames:
+            files.append(os.path.join(root, f))
+        for d in directories:
+            dirs.append(os.path.join(root, d))
+    return (files, dirs)
+
+
+def createProjectFiles(templateDir, destinationDir, projectName):
+    os.mkdir(projectName)
+
+    files, dirs = getAllFilesInDir(templateDir)
+    for srcDir in dirs:
+        destDir = srcDir.replace(TEMPLATE_DIR, projectName)
+        destDir = destDir.replace(PACKAGE_NAME_PLACEHOLDER, projectName)
+        os.mkdir(destDir)
+    for srcFile in files:
+        destFile = srcFile.replace(TEMPLATE_DIR, projectName)
+        destFile = destFile.replace(PACKAGE_NAME_PLACEHOLDER, projectName)
+        shutil.copyfile(srcFile, destFile)
+
+
+def replaceTextInFile(filename, textToReplace, newText):
+    with open(filename) as f:
+        modifiedContents = f.read().replace(textToReplace, newText)
+    with open(filename, 'w') as f:
+        f.write(modifiedContents)
+
+
+def replaceTextRecursively(rootDir, textToReplace, newText):
+    files, dirs = getAllFilesInDir(rootDir)
+    for fname in files:
+        replaceTextInFile(fname, textToReplace, newText)
+
+
+def createProjectBoilerplate(templateDir, projectName, projectDir=None):
+    if not projectDir:
+        projectDir = projectName
+    if os.path.isdir(projectDir):
+        raise ValueError('Directory "{:s}" already exists'.format(projectDir))
+    if not PACKAGE_NAME_REGEX.match(projectName):
+        raise ValueError('"{:s}" is not a valid package name'.format(projectName))
+
+    createProjectFiles(templateDir, projectDir, projectName)
+    replaceTextRecursively(projectDir, PACKAGE_NAME_PLACEHOLDER, projectName)
+
+
+if __name__ == '__main__':
+    # Parse command lime arguments
+    parser = argparse.ArgumentParser(description='todo')
+    parser.add_argument('package_name', type=str,
+                        help='TODO')
+    parser.add_argument('-o', '--outputDir', dest='output_dir', type=str,
+                        help='directory output ')
+    args = parser.parse_args()
+
+    # Generate package
+    createProjectBoilerplate(TEMPLATE_DIR, args.package_name, args.output_dir)
